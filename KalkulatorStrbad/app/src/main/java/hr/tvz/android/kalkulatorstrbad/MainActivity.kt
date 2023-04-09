86cd94d4f8a9ee054d650c688b079cf8b6ba3f89
LukaStrbad <s.luka.1308@gmail.com>
2023-04-09 14:00:52 +0200

    Add currency conversion and state saving/restoring

--- KalkulatorStrbad/app/src/main/java/hr/tvz/android/kalkulatorstrbad/MainActivity.kt
+++ KalkulatorStrbad/app/src/main/java/hr/tvz/android/kalkulatorstrbad/MainActivity.kt
@@ -2,11 +2,12 @@ package hr.tvz.android.kalkulatorstrbad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.lifecycle.coroutineScope
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import hr.tvz.android.kalkulatorstrbad.databinding.ActivityMainBinding
 import hr.tvz.android.kalkulatorstrbad.model.ExchangeRate
 import io.ktor.client.*
@@ -16,9 +17,8 @@ import kotlinx.coroutines.launch
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
-class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    val tecajUrl = "https://api.hnb.hr/tecajn-eur/v3"
     val exchangeRates = mutableListOf<ExchangeRate>()
     val currencies = mutableListOf("EUR")
 
@@ -32,46 +32,110 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         binding.convertButton.setOnClickListener { convert(it) }
 
-        setCurrencySpinnerAdapter(currencies)
-        binding.currencyFromSpinner.onItemSelectedListener = this
-        binding.currencyToSpinner.onItemSelectedListener = this
+        // Restore state
+        savedInstanceState?.getString(AMOUNT_KEY)?.let {
+            binding.currencyAmountTextInput.setText(it)
+        }
+        savedInstanceState?.getString(CURRENCY_FROM_KEY)?.let { currencyFrom = it }
+        savedInstanceState?.getString(CURRENCY_TO_KEY)?.let { currencyTo = it }
+        savedInstanceState?.getString(CONVERSION_TEXT_KEY)?.let {
+            binding.conversionTextView.text = it
+        }
+
+        setCurrencySpinnerAdapter(currencies, currencyFrom, currencyTo)
+
+        binding.currencyAmountTextInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.currencyAmountTextInput.error = null
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+
+        })
+
+        (binding.currencyFromSpinner.editText as? MaterialAutoCompleteTextView)
+            ?.setOnItemClickListener { _, _, _, _ ->
+                currencyFrom = binding.currencyFromSpinner.editText?.text.toString()
+            }
+        (binding.currencyToSpinner.editText as? MaterialAutoCompleteTextView)
+            ?.setOnItemClickListener { _, _, _, _ ->
+                currencyTo = binding.currencyToSpinner.editText?.text.toString()
+            }
     }
 
-    private fun setCurrencySpinnerAdapter(currencies: Iterable<String>) = ArrayAdapter(
-        this, android.R.layout.simple_spinner_item, currencies.toList()
-    ).also { arrayAdapter ->
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.currencyFromSpinner.adapter = arrayAdapter
-        binding.currencyToSpinner.adapter = arrayAdapter
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(AMOUNT_KEY, binding.currencyAmountTextInput.text?.toString())
+        outState.putString(
+            CURRENCY_FROM_KEY,
+            binding.currencyFromSpinner.editText?.text?.toString()
+        )
+        outState.putString(CURRENCY_TO_KEY, binding.currencyToSpinner.editText?.text?.toString())
+        outState.putString(CONVERSION_TEXT_KEY, binding.conversionTextView.text?.toString())
+    }
+
+    private fun setCurrencySpinnerAdapter(
+        currencies: Iterable<String>,
+        currencyFrom: String,
+        currencyTo: String
+    ) {
+        (binding.currencyFromSpinner.editText as? MaterialAutoCompleteTextView)?.apply {
+            setSimpleItems(currencies.toList().toTypedArray())
+            setText(currencyFrom, false)
+        }
+
+        (binding.currencyToSpinner.editText as? MaterialAutoCompleteTextView)?.apply {
+            setSimpleItems(currencies.toList().toTypedArray())
+            setText(currencyTo, false)
+        }
     }
 
     override fun onStart() {
         super.onStart()
         val client = HttpClient()
         lifecycle.coroutineScope.launch {
-            val response = client.request(tecajUrl)
+            val response = client.request(TECAJ_URL)
             exchangeRates.addAll(Json.decodeFromString<List<ExchangeRate>>(response.body()))
             currencies.addAll(exchangeRates.map { it.valuta })
-            setCurrencySpinnerAdapter(currencies)
+            setCurrencySpinnerAdapter(currencies, currencyFrom, currencyTo)
         }
     }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-        currencyFrom = binding.currencyFromSpinner.selectedItem.toString()
-        currencyTo = binding.currencyToSpinner.selectedItem.toString()
-    }
-
-    override fun onNothingSelected(p0: AdapterView<*>?) {}
 
     private fun convert(view: View) {
         val amount = binding.currencyAmountTextInput.text.toString().toDoubleOrNull()
 
         if (amount == null) {
             Snackbar.make(view, getString(R.string.enter_amount), Snackbar.LENGTH_LONG).show()
+            binding.currencyAmountTextInput.error = getString(R.string.enter_amount)
             return
         }
 
+        var converted = amount
+
+        if (currencyFrom != "EUR") {
+            val exchangeRate = exchangeRates.find { it.valuta == currencyFrom } ?: return
+
+            converted /= exchangeRate.srednjiTecaj.replace(',', '.').toDouble()
+        }
+
+        if (currencyTo != "EUR") {
+            val exchangeRate = exchangeRates.find { it.valuta == currencyTo } ?: return
+
+            converted *= exchangeRate.srednjiTecaj.replace(',', '.').toDouble()
+        }
+
         binding.conversionTextView.text =
-            getString(R.string.conversion_display, 1.0, currencyFrom, 0.98, currencyTo)
+            getString(R.string.conversion_display, amount, currencyFrom, converted, currencyTo)
+    }
+
+    companion object {
+        private const val TECAJ_URL = "https://api.hnb.hr/tecajn-eur/v3"
+        private const val AMOUNT_KEY = "amount"
+        private const val CURRENCY_FROM_KEY = "currencyFrom"
+        private const val CURRENCY_TO_KEY = "currencyTo"
+        private const val CONVERSION_TEXT_KEY = "conversionText"
     }
 }
